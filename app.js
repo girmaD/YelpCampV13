65475a651ecb52b53881751a1a586ddc21d32f41
Girma Ebssa <girma.ebssa@gmail.com>
2020-09-06 09:57:00 -0700

    edit mongoose connect and hide info

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const express        = require('express');
-const flash          = require('connect-flash');
-const bodyParser     = require('body-parser');
-const mongoose       = require("mongoose");
-const Campground     = require('./models/campground');
-const User           = require('./models/user');
-const seedDB         = require('./seeds');
-const Comment        = require('./models/comment');
-const passport       = require('passport');
-const localStrategy  = require('passport-local');
+const express = require('express');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const User = require('./models/user');
+const seedDB = require('./seeds');
+const Comment = require('./models/comment');
+const passport = require('passport');
+const localStrategy = require('passport-local');
 const methodOverride = require('method-override');
 
-const commentRoutes     = require('./routes/comments'),
-	  campgroundsRoutes = require('./routes/campgrounds'),
-	  indexRoutes       = require('./routes/index');
+const commentRoutes = require('./routes/comments'),
+	campgroundsRoutes = require('./routes/campgrounds'),
+	indexRoutes = require('./routes/index');
 
-const app               = express();
+const app = express();
 
 // seedDB(); //seed the database
 
 //PASSPORT CONFIGURATION
-app.use(require('express-session')({
-	secret: 'I have the best girl on the planet!',
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+	require('express-session')({
+		secret: 'I have the best girl on the planet!',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 app.use(flash());
 
@@ -37,29 +39,29 @@ passport.deserializeUser(User.deserializeUser());
 
 // connectint mongoose with mongodb
 //mongoose.connect("mongodb://localhost/yelp_camp_v12Deployed", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://girmaDE:ethiopia@cluster0.stvb7.mongodb.net/YelpCamp_v12?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.use(bodyParser.urlencoded({extended: true}));
+const url = process.env.DATABASEURL || 'mongodb://localhost/YelpCamp_v12';
+mongoose.connect(url);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 //passing req.user and req.flash('error') to all route files
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
-	res.locals.error     = req.flash('error');
-	res.locals.success     = req.flash('success');
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
 	next();
 });
 
 //Requiring routes
 app.use('/campgrounds/:id/comments', commentRoutes);
-app.use("/", indexRoutes);
-app.use('/campgrounds',  campgroundsRoutes);
-
-
+app.use('/', indexRoutes);
+app.use('/campgrounds', campgroundsRoutes);
 
 // Initiating the server
-app.listen(3000, function(){
-	console.log("YelpCamp server in use");
+app.listen(3000, function() {
+	console.log('YelpCamp server in use');
 });
 app.listen(process.env.PORT, process.env.IP);
